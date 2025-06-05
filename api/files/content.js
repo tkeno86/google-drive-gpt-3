db293af158b4d748536d871e43c1b6e5cd3b0395
tkeno86 <thomas.kennedy986@gmail.com>
2025-06-05 21:19:23 +0100

    Update content.js

--- api/files/content.js
+++ api/files/content.js
@@ -5,8 +5,8 @@ export default async function handler(req, res) {
   // ✅ New hardcoded access token — for testing only
   const accessToken = "ya29.a0AW4Xtxi7A84W8zo5C8hHKJJsP7UBcg2CJUSJVMppdxEx9onYeO4F7cijP3KKlmWcKxWQRpqh-xdW0Ek-YPrG7ZU62b8baY4ORkxg8e0xPlzt61JMBDXlGj4PA7swBfPyrhy1cixAKz1M9_e8izh6Riw7tiDNq7s1iW3jDBELaCgYKAV4SARYSFQHGX2Mi6J7VJHSMGW6uEtQYU7jRqQ0175";
 
-  // ✅ Known good Google Drive file ID (ASOS Annual Report 2024)
-  const fileId = "1GuRvtLFfA1fSeKjGVZY5SfEyPtpBZoCOa8Jkt5-vXaU";
+  // ✅ Known good Google Drive file ID (Ferrari Annual Report)
+  const fileId = "1kC7fkhY0aETchfNKXOEhr-9H7J4Rl9fHicna1yevOUY";
 
   try {
     const url = `https://www.googleapis.com/drive/v3/files/${fileId}/export?mimeType=text/plain`;
